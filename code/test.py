e1ff4aba71623427f85eb2a3c0aee473871c8542
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-04-01 21:12:47 +0800

    Update test.py

--- code/test.py
+++ code/test.py
@@ -1,11 +1,9 @@
 import tensorflow as tf
 
 if __name__ == "__main__":
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    assert x_train.shape == (60000, 28, 28)
+    (_, _), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     assert x_test.shape == (10000, 28, 28)
-    assert y_train.shape == (60000,)
     assert y_test.shape == (10000,)
 
-    # Model
+    # Model loading
     # Accuracy
